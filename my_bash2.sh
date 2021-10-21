e297ea71364e545370a2395e44f38cda4aa45f33
DanyloRumiantcev <92931323+DanyloRumiantcev@users.noreply.github.com>
2021-10-21 16:54:05 +0300

    Add files via upload
    Предыдущий файл, но можно уже вводить параметры с консоли

---
+++ my_bash2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Enter adress for search:"
+read adress
+echo "Enter pattern for search:"
+read pattern
+echo "Saved files:"
+find $adress -type f -iname "*$pattern*" | tee list.txt
